2b1ccb5987a563d2070c991a599d1ec0698ba88b
Imagine-Choi <sang8408@naver.com>
2024-02-19 05:03:25 +0900

    feat: 기본 화면 frame & 화면 전환 & stt 기본 구현 (1)

--- app/src/main/java/de/yanneckreiss/mlkittutorial/ui/MainScreen.kt
+++ app/src/main/java/de/yanneckreiss/mlkittutorial/ui/MainScreen.kt
@@ -2,24 +2,50 @@
 
 package de.yanneckreiss.mlkittutorial.ui
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.Icon
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import de.yanneckreiss.cameraxtutorial.R
 import de.yanneckreiss.mlkittutorial.ui.camera.CameraScreen
 import de.yanneckreiss.mlkittutorial.ui.no_permission.NoPermissionScreen
 
+@SuppressLint("SuspiciousIndentation")
 @Composable
 fun MainScreen() {
 
     val cameraPermissionState: PermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
 
-    MainContent(
-        hasPermission = cameraPermissionState.status.isGranted,
-        onRequestPermission = cameraPermissionState::launchPermissionRequest
-    )
+            MainContent(
+            hasPermission = cameraPermissionState.status.isGranted,
+            onRequestPermission = cameraPermissionState::launchPermissionRequest
+        )
+
+
+
 }
 
 @Composable
@@ -33,13 +59,4 @@ private fun MainContent(
     } else {
         NoPermissionScreen(onRequestPermission)
     }
-}
-
-@Preview
-@Composable
-private fun Preview_MainContent() {
-    MainContent(
-        hasPermission = true,
-        onRequestPermission = {}
-    )
 }
