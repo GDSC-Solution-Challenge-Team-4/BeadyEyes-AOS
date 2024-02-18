083d3b6b92ee19a4242aecdd0f51c90a6ed95948
Imagine-Choi <sang8408@naver.com>
2024-02-19 05:06:15 +0900

    feat: 기본 화면 frame & 화면 전환 & stt 기본 구현 (2)

---
+++ app/src/main/java/de/yanneckreiss/mlkittutorial/ui/pointer/PointerScreen.kt
@@ -0,0 +1,96 @@
+package de.yanneckreiss.mlkittutorial.ui.pointer
+
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import de.yanneckreiss.mlkittutorial.ui.pointer.ui.theme.JetpackComposeCameraXMLKitTutorialTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun PointerScreen(modifier: Modifier = Modifier) {
+
+    var showMessage by remember { mutableStateOf(false) }
+
+    val alpha by animateFloatAsState(
+        targetValue = if (showMessage) 1f else 0f,
+        animationSpec = if (showMessage) {
+            tween(durationMillis = 1000, easing = FastOutSlowInEasing)
+        } else {
+            keyframes {
+                durationMillis = 1000
+                1.0f at 0 // fade out 완료
+                0.0f at 1 using FastOutSlowInEasing // fade out 시작
+
+            }
+        }, label = ""
+    )
+
+    LaunchedEffect(Unit) {
+        showMessage = true
+        delay(2000)
+        showMessage = false
+    }
+
+    if (showMessage) {
+        Box(
+            modifier = Modifier
+                .alpha(alpha)
+                .graphicsLayer(alpha = alpha)
+                .fillMaxSize()
+                .padding(top = 50.dp),
+            contentAlignment = Alignment.TopCenter // 세로 정렬을 맨 위로 설정
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Gray, shape = RoundedCornerShape(20.dp))
+                    .padding(top = 5.dp, start = 10.dp, end = 10.dp, bottom = 10.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Pointer Screen",
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+
+    Text(
+        text = "Pointer",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PointerPreview() {
+    JetpackComposeCameraXMLKitTutorialTheme {
+        PointerScreen()
+    }
+}
