2050ff87f8f408bd2eff9c606b5bf9821cd22dd3
Imagine-Choi <sang8408@naver.com>
2024-02-22 01:37:21 +0900

    feat: 화면 캡쳐 error commit 2

--- app/src/main/java/de/yanneckreiss/mlkittutorial/ui/money/ui/CameraCapture.kt
+++ app/src/main/java/de/yanneckreiss/mlkittutorial/ui/money/ui/CameraCapture.kt
@@ -1,21 +1,28 @@
 package de.yanneckreiss.mlkittutorial.ui.money.ui
 
 import android.content.Context
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.camera.core.CameraProvider
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -23,6 +30,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LifecycleResumeEffect
+import androidx.lifecycle.compose.LifecycleStartEffect
 import com.google.common.util.concurrent.ListenableFuture
 import java.util.concurrent.Executor
 
@@ -35,36 +44,49 @@ fun CameraContentMoney(context: Context, modifier: Modifier = Modifier) {
 
     val imageCaptureExecutor = ContextCompat.getMainExecutor(context)
     val imageCapture = ImageCapture.Builder()
+        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
         .build()
 
-    Column {
-        CameraPreview(cameraProviderFuture, imageCapture, lifecycleOwner)
-        CaptureButton(
-            onClick = { captureImage(context, imageCapture = imageCapture, imageCaptureExecutor) },
-            modifier = Modifier.padding(16.dp)
-        )
+    Box {
+        CameraPreview(imageCaptureExecutor,cameraProviderFuture, imageCapture, lifecycleOwner)
+        Box(
+            modifier = Modifier,
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            CaptureButton(
+                onClick = {
+                    captureImage(
+                        context,
+                        imageCapture = imageCapture,
+                        imageCaptureExecutor
+                    )
+                },
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
 
 @Composable
 fun CameraPreview(
+    imageCaptureExecutor: Executor,
     cameraProviderFuture: ListenableFuture<ProcessCameraProvider>,
     imageCapture: ImageCapture,
     lifecycleOwner: LifecycleOwner,
     modifier: Modifier = Modifier,
 ) {
     val context = LocalContext.current
+
     val previewView = remember {
         PreviewView(context).apply {
-            layoutParams = LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
             implementationMode = PreviewView.ImplementationMode.COMPATIBLE
             scaleType = PreviewView.ScaleType.FILL_START
         }
     }
-    val previewUseCase = remember(previewView) {
+
+    val cameraProvider = cameraProviderFuture.get()
+
+    val previewUseCase = remember() {
         Preview.Builder()
             .build()
             .also {
@@ -74,17 +96,35 @@ fun CameraPreview(
     val cameraSelector = remember {
         CameraSelector.DEFAULT_BACK_CAMERA
     }
-    val cameraProvider = cameraProviderFuture.get()
-    LaunchedEffect(previewUseCase, cameraSelector) {
-        cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, imageCapture, previewUseCase)
+
+    val imageAnalysis = ImageAnalysis.Builder()
+        .build()
+        .apply {
+            setAnalyzer(imageCaptureExecutor, ImageAnalysis.Analyzer { image ->
+                image.close()
+            })
+        }
+
+
+    LifecycleStartEffect(previewUseCase, cameraSelector, lifecycleOwner = lifecycleOwner) {
+        try {
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, cameraSelector, previewUseCase, imageCapture, imageAnalysis)
+            Log.d("MainActivity25", "바인딩 성공")
+        }catch (e: Exception){
+            Log.d("MainActivity25", "바인딩 실패 $e")
+        }
+        onStopOrDispose {
+            cameraProvider.unbindAll()
+        }
     }
 
     AndroidView(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+            .fillMaxSize(),
         factory = { previewView }
     )
+    ContextCompat.getMainExecutor(context)
 }
 
 
@@ -96,6 +136,7 @@ fun CaptureButton(
     Button(
         onClick = onClick,
         modifier = modifier
+            .fillMaxWidth()
     ) {
         Text("Capture")
     }
@@ -107,20 +148,22 @@ fun captureImage(
     imageCaptureExecutor: Executor
 ) {
     val file = createTempFile(context)
+    Log.d("MainActivity24", "${createTempFile(context)}")
     val outputFileOptions =
         ImageCapture.OutputFileOptions.Builder(file).build()
+
     imageCapture.takePicture(
         outputFileOptions,
         imageCaptureExecutor,
         object : ImageCapture.OnImageSavedCallback {
             override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                android.util.Log.d("MainActivity", "Image saved: ${file.absolutePath}")
+                Log.d("MainActivity22", "Image saved: ${file.absolutePath}")
                 // Add your code here
             }
 
             override fun onError(exception: androidx.camera.core.ImageCaptureException) {
-                android.util.Log.e(
-                    "MainActivity",
+                Log.e(
+                    "MainActivity23",
                     "Error capturing image: ${exception.message}",
                     exception
                 )
@@ -132,7 +175,7 @@ fun captureImage(
 
 fun createTempFile(context: Context): java.io.File {
     val timeStamp = java.text.SimpleDateFormat("yyyyMMdd_HHmmss", java.util.Locale.getDefault())
-        .format(java.util.Date())
+                                .format(java.util.Date())
     val imageFileName = "JPEG_$timeStamp.jpg"
     val storageDir = context.getExternalFilesDir(android.os.Environment.DIRECTORY_PICTURES)
     return java.io.File.createTempFile(imageFileName, ".jpg", storageDir)
